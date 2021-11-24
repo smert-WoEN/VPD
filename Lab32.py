c3d41c37c9edbbbaa3f2ea9912afae0d3fe23fb2
DmitriiDenisov <denisdmit03@gmail.com>
2021-11-24 21:13:30 +0300

    lab3 add path 2

---
+++ Lab32.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+from ev3dev.ev3 import *
+import time
+import math
+
+
+def sign(number):
+    if number > 0.0:
+        return 1.0
+    if number < 0.0:
+        return -1.0
+    return 0.0
+
+
+mB = LargeMotor(OUTPUT_B)
+mC = LargeMotor(OUTPUT_C)
+usSensor1 = UltrasonicSensor('in1').mode('US-DIST-CM')
+usSensor2 = UltrasonicSensor('in2').mode('US-DIST-CM')
+fh = open("data.txt", "w")
+fh.write('0 ' + '0 ' + '\n')
+startTime = time.time()
+timeOld = 0
+
+
+h = float(12.0)
+target = float(50.0)
+kP = 1.0
+kI = 0.0
+kD = 0.0
+maxI = 0
+I = 0.0
+erorrOld = 0.0
+speed = 50
+try:
+    while True:
+        currentTime = time.time() - startTime
+        d1 = usSensor1.value()
+        d2 = usSensor2.value()
+        d = 0.5 * h * (d1 + d2) * math.sqrt(1.0 / (h * h + (d1 -d2) * (d1 -d2)))
+        deltaTime = currentTime - timeOld
+        timeOld = currentTime
+        fh.write(str(currentTime) + ' ' + str(d) + ' \n')
+        erorr = target - d
+        P = erorr * kP
+        I += erorr * kI * deltaTime
+        if abs(I) > maxI:
+            I = sign(I) * maxI
+        D = (erorr - erorrOld) * kD / deltaTime
+        erorrOld = erorr
+        power = P + I + D
+        if abs(power) > 100 - speed:
+            power = sign(power) * (100 - speed)
+        mB.run_direct(duty_cycle_sp = speed + power)
+        mC.run_direct(duty_cycle_sp = speed - power)
+
+finally:
+    mB.stop(stop_action = 'brake')
+    mC.stop(stop_actiom = 'brake')
+    fh.close()

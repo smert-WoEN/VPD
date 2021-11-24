5365e42cd7b94ab62799a82240edfc29b3b948bf
DmitryDenisov <denisdmit03@gmail.com>
2021-11-24 11:14:12 +0300

    lab3

--- Lab3.py
+++ Lab3.py
@@ -6,30 +6,9 @@ mA = LargeMotor('outA')
 mA.position = current_time = start_time = 0
 
 fh = open("data.txt", "w")
-fh.write('0 ' + '0 ' + '0 ' + '0' + '\n')
+fh.write('0 ' + '0 ' + '0' + '\n')
 
 start_time = time.time()
-kP = 0.0
-kI = 0.0
-kD = 0.0
-kV = 0.0
-kA = 0.0
-kS = 0.0
-maxI = 32767
-P = 0.0
-I = 0.0
-D = 0.0
-V = 0.0
-A = 0.0
-S = 0.0
-power = 0.0
-timeOld = 0.0
-timeDelta = 0.0
-currentTime = 0.0
-currentVelocity = 0.0
-velocityError = 0.0
-velocityErrorOld = 0.0
-velocityTargetOld = 0.0
 
 
 def sign(number):
@@ -40,44 +19,51 @@ def sign(number):
     return 0.0
 
 
-def update(target):
-    global timeDelta, timeOld, currentTime, currentVelocity, velocityErrorOld, velocityError, velocityTargetOld, power, P, I, D, V, A, S
-    timeDelta = currentTime - timeOld
-    timeOld = currentTime
-    currentVelocity = mA.speed
-    if target != 0.0:
-        velocityError = target - currentVelocity
-        P = velocityError * kP
-        D = (velocityError - velocityErrorOld) / timeDelta
-        I += (kI * velocityError) * timeDelta
-        if target == 0.0:
-            I = .0
-        if abs(I) > maxI:
+kP = 1.0
+kI = 0.0
+kD = 0.0
+kS = 0.0
+
+maxI = float(50.0)
+I = 0.0
+maxSpeed = 100.0
+errorOld = 0.0
+timeOld = 0.0
+
+
+def reg(target):
+    global I, errorOld, timeOld
+    timeDelta = (time.time() - start_time) - timeOld
+    timeOld = time.time() - start_time
+    pos = mA.position
+    error = target - pos
+    power = 0.0
+    P = 0.0
+    D = 0.0
+    S = 0.0
+    if target != pos:
+        P = error * kP / timeDelta
+        I += (error * kI) * timeDelta
+        if I > maxI:
             I = sign(I) * maxI
-        V = kV * target
-        A = kA * (target - velocityTargetOld) / timeDelta
-        S = kS * sign(target)
-        power = P + I + D + V + A + S
-        velocityErrorOld = velocityError
-        velocityTargetOld = target
+        D = (error - errorOld) * kD
+        S = kS * sign(error)
+        power = P + I + D + S
+        errorOld = error
     else:
-        velocityError = 0.0
         power = 0.0
-        velocityErrorOld = 0.0
+        errorOld = 0.0
         I = 0.0
-        velocityTargetOld = 0.0
+    power = min(abs(power), maxSpeed) * sign(power)
     return power
 
 
-goto = int(100)
+goto = int(200)
 try:
     while True:
-        currentTime = time.time() - start_time
-        if current_time > 5:
-            break
-        else:
-            mA.run_direct(duty_cycle_sp=round(update(goto)))
-            fh.write(str(current_time) + ' ' + str(mA.position) + ' ' + str(goto) + ' ' + str(mA.speed) + '\n')
+        current_time = time.time() - start_time
+        mA.run_direct(duty_cycle_sp = round(reg(goto)))
+        fh.write(str(current_time) + ' ' + str(mA.position) + ' ' + str(mA.speed) + ' ' + str(goto) + '\n')
 finally:
-    mA.stop(stop_action='brake')
+    mA.stop(stop_action = 'brake')
     fh.close()
